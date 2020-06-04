788f93176aef137bff0d14bcdffeca69c6f3773c
Jasper Caballero <jvicaballero@gmail.com>
2020-06-04 14:26:55 -0400

    Connected to API, also added textfield and button

--- src/components/citySearch.js
+++ src/components/citySearch.js
@@ -1,15 +1,45 @@
 import React, {Component} from 'react';
-
+import axios from 'axios';
 
 
 class CitySearch extends Component{
     constructor(props)
     {
-    super(props)
-    this.state = {
-        zipcode = [],
+     super(props);
+        this.state = {
+            zipcode : null
+         };
     }
+
+    componentDidMount(){
+        axios.get('http://ctp-zip-api.herokuapp.com').then((response) => {
+            const data = response.data;
+            console.log(data)
+            const newZipObj = {
+                text: data.test,
+            }
+
+            this.setState({zipcode: newZipObj});
+        })
+            .catch((error => console.log(error)));
+
+            
+    }
+
+    
+
+    render()
+    {
+        return(
+            <div>
+                <p>{this.state.zipcode && this.state.zipcode.text}</p>
+                <input type="text" placeholder="Type in a city. Ex)Akron"/>
+                <button type="submit">Submit</button>
+            </div>
+        );
     }
 }
 
 export default CitySearch
+
+//this.state.zipcode&&this.state.zipcode.text gives it time to render
